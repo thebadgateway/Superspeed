1682584e54bf835e9652eb2d76e5812803ce4b36
thebadgateway <45717841+thebadgateway@users.noreply.github.com>
2019-10-17 01:44:24 +0800

    Update superspeed.sh

--- superspeed.sh
+++ superspeed.sh
@@ -57,13 +57,13 @@ echo "# Author: Oldking <oooldking@gmail.com>                     #"
 echo "# Github: https://github.com/oooldking                      #"
 echo "#############################################################"
 echo
-echo "测试服务器到"
-echo -ne "1.中国电信 2.中国联通 3.中国移动 4.本地默认 5.全面测速"
+echo "Test server to"
+echo -ne "1. China Telecom 2. China Unicom 3. China mobile 4. Local default 5. Comprehensive speed measurement"
 
 while :; do echo
-        read -p "请输入数字选择： " telecom
+        read -p "Please enter a number selection: " telecom
         if [[ ! $telecom =~ ^[1-5]$ ]]; then
-                echo "输入错误! 请输入正确的数字!"
+                echo "Input error! Please enter the correct number!"
         else
                 break   
         fi
@@ -71,227 +71,227 @@ done
 
 if [[ ${telecom} == 1 ]]; then
         telecomName="电信"
-        echo -e "\n选择最靠近你的方位"
-    echo -ne "1.北方 2.南方"
+        echo -e "\nChoose the position closest to you"
+    echo -ne "1. North 2. South"
     while :; do echo
-            read -p "请输入数字选择： " pos
+            read -p "Please enter a number selection: " pos
             if [[ ! $pos =~ ^[1-2]$ ]]; then
-                    echo "输入错误! 请输入正确的数字!"
+                    echo "Input error! Please enter the correct number!"
             else
                     break
             fi
     done
-    echo -e "\n选择最靠近你的城市"
+    echo -e "\nChoose the city closest to you"
     if [[ ${pos} == 1 ]]; then
-        echo -ne "1.郑州 2.襄阳"
+        echo -ne "1.Zhengzhou 2.Fuyang"
         while :; do echo
-                read -p "请输入数字选择： " city
+                read -p "Please enter a number selection: " city
                 if [[ ! $city =~ ^[1-2]$ ]]; then
-                        echo "输入错误! 请输入正确的数字!"
+                        echo "Input error! Please enter the correct number!"
                 else
                         break
             fi
         done
         if [[ ${city} == 1 ]]; then
                 num=4595
-                cityName="郑州"
+                cityName="Zhengzhou"
         fi
         if [[ ${city} == 2 ]]; then
                 num=12637
-                cityName="襄阳"
+                cityName="Fuyang"
         fi
     fi
     if [[ ${pos} == 2 ]]; then
-        echo -ne "1.上海 2.杭州 3.南宁 4.南昌 5.长沙 6.深圳 7.重庆 8.成都"
+        echo -ne "1.Shanghai 2.Hangzhou 3.Nanning 4.Nanchang 5.Changsha 6.Shenzhen 7.Chongqing 8.Chengdu"
         while :; do echo
-                read -p "请输入数字选择： " city
+                read -p "Please enter a number selection: " city
                 if [[ ! $city =~ ^[1-8]$ ]]; then
-                        echo "输入错误! 请输入正确的数字!"
+                        echo "Input error! Please enter the correct number!"
                 else
                         break
             fi
         done
         if [[ ${city} == 1 ]]; then
                 num=3633
-                cityName="上海"
+                cityName="Shanghai"
         fi
         if [[ ${city} == 2 ]]; then
                 num=7509
-                cityName="杭州"
+                cityName="Hangzhou"
         fi
         if [[ ${city} == 3 ]]; then
                 num=10305
-                cityName="南宁"
+                cityName="Nanning"
         fi
         if [[ ${city} == 4 ]]; then
                 num=7230
-                cityName="南昌"
+                cityName="Nanchang"
         fi
         if [[ ${city} == 5 ]]; then
                 num=6132
-                cityName="长沙"
+                cityName="Changsha"
         fi
         if [[ ${city} == 6 ]]; then
                 num=5081
-                cityName="深圳"
+                cityName="Shenzhen"
         fi
         if [[ ${city} == 7 ]]; then
                 num=6592
-                cityName="重庆"
+                cityName="Chongqing"
         fi
         if [[ ${city} == 8 ]]; then
                 num=4624
-                cityName="成都"
+                cityName="Chengdu"
         fi
     fi
 fi
 
 if [[ ${telecom} == 2 ]]; then
-        telecomName="联通"
-    echo -ne "\n1.北方 2.南方"
+        telecomName="Unicom"
+    echo -ne "\n1. North 2. South"
     while :; do echo
-            read -p "请输入数字选择： " pos
+            read -p "Please enter a number selection: " pos
             if [[ ! $pos =~ ^[1-2]$ ]]; then
-                    echo "输入错误! 请输入正确的数字!"
+                    echo "Input error! Please enter the correct number!"
             else
                     break
             fi
     done
-    echo -e "\n选择最靠近你的城市"
+    echo -e "\nChoose the city closest to you"
     if [[ ${pos} == 1 ]]; then
-        echo -ne "1.沈阳 2.长春 3.哈尔滨 4.天津 5.济南 6.北京 7.郑州 8.西安 9.太原 10.宁夏 11.兰州 12.西宁"
+        echo -ne "1.Shenyang 2.Changchun 3.Harbin 4.Tianjin 5.Jinan 6.Beijing 7.Zhengzhou 8.Xi'an 9.Taiyuan 10.Ningxia 11.Lanzhou 12.Xining"
         while :; do echo
-                read -p "请输入数字选择： " city
+                read -p "Please enter a number selection: " city
                 if [[ ! $city =~ ^(([1-9])|(1([0-2]{1})))$ ]]; then
-                        echo "输入错误! 请输入正确的数字!"
+                        echo "Input error! Please enter the correct number!"
                 else
                         break
             fi
         done
         if [[ ${city} == 1 ]]; then
                 num=5017
-                cityName="沈阳"
+                cityName="Shenyang"
         fi
         if [[ ${city} == 2 ]]; then
                 num=9484
-                cityName="长春"
+                cityName="Changchun"
         fi
         if [[ ${city} == 3 ]]; then
                 num=5460
-                cityName="哈尔滨"
+                cityName="Harbin"
         fi
         if [[ ${city} == 4 ]]; then
                 num=5475
-                cityName="天津"
+                cityName="Tianjin"
         fi
         if [[ ${city} == 5 ]]; then
                 num=5039
-                cityName="济南"
+                cityName="Jinan"
         fi
         if [[ ${city} == 6 ]]; then
                 num=5145
-                cityName="北京"
+                cityName="Beijing"
         fi
         if [[ ${city} == 7 ]]; then
                 num=5131
-                cityName="郑州"
+                cityName="Zhengzhou"
         fi
         if [[ ${city} == 8 ]]; then
                 num= 4863
-                cityName="西安"
+                cityName="Xi'an"
         fi
         if [[ ${city} == 9 ]]; then
                 num=12868
-                cityName="太原"
+                cityName="Taiyuan"
         fi
         if [[ ${city} == 10 ]]; then
                 num=5509
-                cityName="宁夏"
+                cityName="Ningxia"
         fi
         if [[ ${city} == 11 ]]; then
                 num=4690
-                cityName="兰州"
+                cityName="Lanzhou"
         fi
         if [[ ${city} == 12 ]]; then
                 num=5992
-                cityName="西宁"
+                cityName="Xining"
         fi
     fi
     if [[ ${pos} == 2 ]]; then
-        echo -ne "1.上海 2.杭州 3.南宁 4.合肥 5.南昌 6.长沙 7.深圳 8.广州 9.重庆 10.昆明 11.成都"
+        echo -ne "1.Shanghai 2.Hangzhou 3.Nanning 4.Hefei 5.Nanchang 6.Changsha 7.Shenzhen 8.Guangzhou 9.Chongqing 10.Guangzhou 11.Chengdu"
         while :; do echo
-                read -p "请输入数字选择： " city
+                read -p "Please enter a number selection: " city
                 if [[ ! $city =~ ^(([1-9])|(1([0-1]{1})))$ ]]; then
-                        echo "输入错误! 请输入正确的数字!"
+                        echo "Input error! Please enter the correct number!"
                 else
                         break
             fi
         done
         if [[ ${city} == 1 ]]; then
                 num=5083
-                cityName="上海"
+                cityName="Shanghai"
         fi
         if [[ ${city} == 2 ]]; then
                 num=5300
-                cityName="杭州"
+                cityName="Hangzhou"
         fi
         if [[ ${city} == 3 ]]; then
                 num=5674
-                cityName="南宁"
+                cityName="Nanning"
         fi
         if [[ ${city} == 4 ]]; then
                 num=5724
-                cityName="合肥"
+                cityName="Hefei"
         fi
         if [[ ${city} == 5 ]]; then
                 num=5079
-                cityName="南昌"
+                cityName="Nanchang"
         fi
         if [[ ${city} == 6 ]]; then
                 num=4870
-                cityName="长沙"
+                cityName="Changsha"
         fi
         if [[ ${city} == 7 ]]; then
                 num=10201
-                cityName="深圳"
+                cityName="Shenzhen"
         fi
         if [[ ${city} == 8 ]]; then
                 num=3891
-                cityName="广州"
+                cityName="Guangzhou"
         fi
         if [[ ${city} == 9 ]]; then
                 num=5726
-                cityName="重庆"
+                cityName="Chongqing"
         fi
         if [[ ${city} == 10 ]]; then
                 num=5103
-                cityName="昆明"
+                cityName="Guangzhou"
         fi
         if [[ ${city} == 11 ]]; then
                 num=2461
-                cityName="成都"
+                cityName="Chengdu"
         fi
     fi
 fi
 
 if [[ ${telecom} == 3 ]]; then
-        telecomName="移动"
-    echo -ne "\n1.北方 2.南方"
+        telecomName="mobile"
+    echo -ne "\n1. North 2. South"
     while :; do echo
-            read -p "请输入数字选择： " pos
+            read -p "Please enter a number selection: " pos
             if [[ ! $pos =~ ^[1-2]$ ]]; then
-                    echo "输入错误! 请输入正确的数字!"
+                    echo "Input error! Please enter the correct number!"
             else
                     break
             fi
     done
-    echo -e "\n选择最靠近你的城市"
+    echo -e "\nChoose the city closest to you"
     if [[ ${pos} == 1 ]]; then
-        echo -ne "1.西安"
+        echo -ne "1.Xi'an"
         while :; do echo
-                read -p "请输入数字选择： " city
+                read -p "Please enter a number selection: " city
                 if [[ ! $city =~ ^[1]$ ]]; then
-                        echo "输入错误! 请输入正确的数字!"
+                        echo "Input error! Please enter the correct number!"
                 else
                         break
             fi
@@ -301,38 +301,38 @@ if [[ ${telecom} == 3 ]]; then
         fi
     fi
     if [[ ${pos} == 2 ]]; then
-        echo -ne "1.上海 2.宁波 3.无锡 4.杭州 5.合肥 6.成都"
+        echo -ne "1.Shanghai 2.Ningbo 3.Wuxi 4.Hangzhou 5.Hefei 6.Chengdu"
         while :; do echo
-                read -p "请输入数字选择： " city
+                read -p "Please enter a number selection: " city
                 if [[ ! $city =~ ^[1-6]$ ]]; then
-                        echo "输入错误! 请输入正确的数字!"
+                        echo "Input error! Please enter the correct number!"
                 else
                         break
             fi
         done
         if [[ ${city} == 1 ]]; then
                 num=4665
-                cityName="上海"
+                cityName="Shanghai"
         fi
         if [[ ${city} == 2 ]]; then
                 num=6715
-                cityName="宁波"
+                cityName="Ningbo"
         fi
         if [[ ${city} == 3 ]]; then
                 num=5122
-                cityName="无锡"
+                cityName="Wuxi"
         fi
         if [[ ${city} == 4 ]]; then
                 num=4647
-                cityName="杭州"
+                cityName="Hangzhou"
         fi
         if [[ ${city} == 5 ]]; then
                 num=4377 
-                cityName="合肥"
+                cityName="Hefei"
         fi
         if [[ ${city} == 6 ]]; then
                 num=4575
-                cityName="成都"
+                cityName="Chengdu"
         fi
     fi
 fi
@@ -353,7 +353,7 @@ result() {
     echo "延迟  : $latency"
     echo "上传  : $upload"
     echo "下载  : $download"
-    echo -ne "\n当前时间: "
+    echo -ne "\n current time: "
     echo $(date +%Y-%m-%d" "%H:%M:%S)
 }
 
@@ -382,18 +382,18 @@ if [[ ${telecom} =~ ^[1-3]$ ]]; then
 
     if [[ ${is_down} ]]; then
         result
-        echo "测试到 ${cityName}${telecomName} 完成！"
+        echo "Test to ${cityName}${telecomName} Complete!"
         rm -rf /tmp/speedtest.py
         rm -rf /tmp/speed.log
     else
-	    echo -e "\n${RED}ERROR:${PLAIN} 当前节点不可用，请更换其他节点，或换个时间段再测试。"
+	    echo -e "\n${RED}ERROR:${PLAIN} The current node is unavailable. Please replace other nodes or test again after changing time periods."
 	fi
 fi
 
 if [[ ${telecom} == 4 ]]; then
     python /tmp/speedtest.py | tee /tmp/speed.log
     result
-    echo "本地测试完成！"
+    echo "Local testing is complete!"
     rm -rf /tmp/speedtest.py
     rm -rf /tmp/speed.log
 fi
@@ -402,18 +402,18 @@ if [[ ${telecom} == 5 ]]; then
 	echo ""
 	printf "%-14s%-18s%-20s%-12s\n" "Node Name" "Upload Speed" "Download Speed" "Latency"
 	start=$(date +%s) 
-    speed_test '6435' '襄阳电信'
-    speed_test '7509' '昆明电信'
-	speed_test '3633' '上海电信'
-	speed_test '4624' '成都电信'
-	speed_test '5017' '沈阳联通'
-	speed_test '4863' '西安联通'
-	speed_test '5083' '上海联通'
-	speed_test '5726' '重庆联通'
-	speed_test '5292' '西安移动'
-	speed_test '16314' '山东移动'
-    speed_test '6715' '宁波移动'
-	speed_test '4575' '成都移动'
+    speed_test '6435' 'Fuyang Telecom'
+    speed_test '7509' 'Kunming Telecom'
+    speed_test '3633' 'Shanghai Telecom'
+    speed_test '4624' 'Chengdu Telecom'
+    speed_test '5017' 'Shenyang Unicom'
+    speed_test '4863' 'Xian Unicom'
+    speed_test '5083' 'Shanghai Unicom'
+    speed_test '5726' 'Chongqing Unicom'
+    speed_test '5292' 'Xian Mobile'
+    speed_test '16314' 'Shandong Mobile'
+    speed_test '6715' 'Ningbo Mobile'
+    speed_test '4575' 'Chengdu Mobile'
 	end=$(date +%s)  
 	rm -rf /tmp/speedtest.py
 	echo ""
@@ -421,11 +421,11 @@ if [[ ${telecom} == 5 ]]; then
 	if [[ $time -gt 60 ]]; then
 		min=$(expr $time / 60)
 		sec=$(expr $time % 60)
-		echo -ne "花费时间：${min} 分 ${sec} 秒"
+		echo -ne "Spend time: ${min} 分 ${sec} 秒"
 	else
-		echo -ne "花费时间：${time} 秒"
+		echo -ne "Spend time:${time} 秒"
 	fi
-	echo -ne "\n当前时间: "
+	echo -ne "\n current time:"
     echo $(date +%Y-%m-%d" "%H:%M:%S)
-	echo "全面测试完成！"
+	echo "Comprehensive testing is complete!"
 fi
